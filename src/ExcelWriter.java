4a42f1d5f469ec6db6141b8fb70ad28006d72932
Uwera Ntaganzwa <nnuwera@gmail.com>
2019-03-23 11:35:10 -0300

    Cleaned Up excelwriter & GUI

--- src/ExcelWriter.java
+++ src/ExcelWriter.java
@@ -9,34 +9,14 @@ public class ExcelWriter {
 	/*
 	 * This part was for testing
 	public static void main(String[] args) throws IOException {
-		/*
-		HSSFWorkbook workbook = new HSSFWorkbook();
-		HSSFSheet sheet = workbook.createSheet("Sheet1");
-		HSSFRow row = sheet.createRow(0);
-		HSSFCell cell = row.createCell(0);
-		HSSFCell cell2 = row.createCell(1);
-		HSSFCell cell3 = row.createCell(2);
-		cell.setCellValue("1, Cell");
-		cell2.setCellValue("2, Cell");
-		cell3.setCellValue("3, Cell");
-		
-		workbook.write(new FileOutputStream("pl.xsl"));
-		workbook.close();
-		
-		
 		ArrayList<Course> list = new ArrayList<>();
 		list.add(new Course("CS2043", "Software Engineering"));
 		list.add(new Course("CS2263", "Systems Software Devt"));
 		list.add(new Course("CS2383", "Algorithms"));
 		
 		writeToExcel(list);
-		
-		
 	}*/
 	
-	//Should this method take in a filename? That would mean that they create a new excel file.
-	//I guess we could also create a new file.
-	//file not found exception would help depending on what we decide. Also, we can add a customized message
 	public static void writeToExcel(ArrayList<Course> sortedList) throws FileNotFoundException, IOException {
 		HSSFWorkbook workbook = new HSSFWorkbook();
 		HSSFSheet sheet = workbook.createSheet("Raw List");
@@ -47,8 +27,7 @@ public class ExcelWriter {
 		courseName.setCellValue("Course Name");
 		
 		int n = 0, m = 0;
-		//A row iterator could be a better design choice.
-		//This part of the method will therefore be improved soon.
+		//Is it possible to replace this with a row iterator even though the for-each loop works fine?
 		for(Course courseIn: sortedList) {
 			n++;
 			HSSFRow nextRow = sheet.createRow(n);
