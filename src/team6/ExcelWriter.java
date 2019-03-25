7fb9027ee722dd89566efd24211cae6c00ac88c4
Uwera <nnuwera@gmail.com>
2019-03-25 12:12:17 -0300

    Replaced GUI & Excel Writer

--- src/team6/ExcelWriter.java
+++ src/team6/ExcelWriter.java
@@ -1,45 +1,36 @@
-package team6;
+/**
+ * @author Uwera Ntaganzwa
+ */
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-
 import org.apache.poi.hssf.usermodel.*;
 
 public class ExcelWriter {
-	/*
-	 * This part was for testing
-	public static void main(String[] args) throws IOException {
-		/*
-		HSSFWorkbook workbook = new HSSFWorkbook();
-		HSSFSheet sheet = workbook.createSheet("Sheet1");
-		HSSFRow row = sheet.createRow(0);
-		HSSFCell cell = row.createCell(0);
-		HSSFCell cell2 = row.createCell(1);
-		HSSFCell cell3 = row.createCell(2);
-		cell.setCellValue("1, Cell");
-		cell2.setCellValue("2, Cell");
-		cell3.setCellValue("3, Cell");
-		
-		workbook.write(new FileOutputStream("pl.xsl"));
-		workbook.close();
-		
-		
-		ArrayList<Course> list = new ArrayList<>();
-		list.add(new Course("CS2043", "Software Engineering"));
-		list.add(new Course("CS2263", "Systems Software Devt"));
-		list.add(new Course("CS2383", "Algorithms"));
-		
-		writeToExcel(list);
-		
-		
-	}*/
+	private static ArrayList<String> createdFiles = new ArrayList<>();
 	
-	//Should this method take in a filename? That would mean that they create a new excel file.
-	//I guess we could also create a new file.
-	//file not found exception would help depending on what we decide. Also, we can add a customized message
+	//Can this be replace with a file.exists method?		
+	public static boolean findFile(String name){
+		boolean success = false;
+		for(String names : createdFiles) {
+			if(names == "RawList.xsl") {
+				//then the file exists and can be written to
+				success = true;
+			}
+		}
+		if(success == false) {
+			String fName = "RawList.xsl";
+			createdFiles.add(fName);
+			success = true;
+		}
+		return success;
+		
+		
+	}
 	public static void writeToExcel(ArrayList<Course> sortedList) throws FileNotFoundException, IOException {
+		findFile("RawList.xsl");
 		HSSFWorkbook workbook = new HSSFWorkbook();
 		HSSFSheet sheet = workbook.createSheet("Raw List");
 		HSSFRow row = sheet.createRow(0);
@@ -49,8 +40,7 @@ public class ExcelWriter {
 		courseName.setCellValue("Course Name");
 		
 		int n = 0, m = 0;
-		//A row iterator could be a better design choice.
-		//This part of the method will therefore be improved soon.
+		//Is it possible to replace this with a row iterator even though the for-each loop works fine?
 		for(Course courseIn: sortedList) {
 			n++;
 			HSSFRow nextRow = sheet.createRow(n);
@@ -62,6 +52,32 @@ public class ExcelWriter {
 		
 		workbook.write(new FileOutputStream("RawList.xsl"));
 		workbook.close();
-		System.out.println("Courses have been successfully copied to the Raw List sheet.");
+		//System.out.println("Courses have been successfully copied to the Raw List sheet.");
+	}
+	
+	public static void writeToExcel(ArrayList<Course> sortedList, String fileName) throws FileNotFoundException, IOException {
+		findFile("fileName");
+		HSSFWorkbook workbook = new HSSFWorkbook();
+		HSSFSheet sheet = workbook.createSheet("Raw List");
+		HSSFRow row = sheet.createRow(0);
+		HSSFCell courseNum = row.createCell(0);
+		HSSFCell courseName = row.createCell(1);
+		courseNum.setCellValue("Course Number");
+		courseName.setCellValue("Course Name");
+		
+		int n = 0, m = 0;
+		//Is it possible to replace this with a row iterator even though the for-each loop works fine?
+		for(Course courseIn: sortedList) {
+			n++;
+			HSSFRow nextRow = sheet.createRow(n);
+			HSSFCell cell = nextRow.createCell(m);
+			HSSFCell nextCell = nextRow.createCell(m+1);
+			cell.setCellValue(courseIn.getCourseNum());
+			nextCell.setCellValue(courseIn.getCourseName());
+		}
+		
+		workbook.write(new FileOutputStream(fileName));
+		workbook.close();
+		//System.out.println("Courses have been successfully copied to the Raw List sheet.");
 	}
 }
--- src/team6/GUI.java
+++ src/team6/GUI.java
@@ -1,49 +1,73 @@
-package team6;
-
+/**
+ * @author Uwera Ntaganzwa
+ */
+import javax.swing.*; 
+import java.awt.event.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 
-import javax.swing.*;
-
-public class GUI {
-
+public class GUI extends JFrame implements ActionListener {
+		private static JLabel message;
+		private static JLabel message2;
+		ArrayList<Course> sortedList = new ArrayList<>();
+		GUI(){}
 	    public static void main(String args[]){
 	       JFrame frame = new JFrame("Student Transcript Analyser");
-	       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	       frame.setSize(750,500);
-	       JButton uploadButton = new JButton("Upload Transcript");
-	       uploadButton.setBounds(50, 150, 100, 30);
-	       frame.add(uploadButton);
-	       frame.setVisible(true);
-	       
-	       
-	       
-	       	//just threw my min method code in here to have the system run. 
-	       	//TODO: couple this all with gui elements
-	       	File directory = null;
-			directory = TranscriptReader.getDirectory();
-			
-	        File[] transcriptSet = directory.listFiles();
-	        System.out.println("Transcript count:" + transcriptSet.length);
-	        TranscriptReader.parseTranscripts(transcriptSet);
+	       JPanel panel = new JPanel();
+	       frame.setSize(400,350);
+	       frame.add(panel);
 
-	        //prints to console
-	        System.out.println(CourseList.printRawList());
-	        
-	        //TODO: add option to print to file
-	        
-	        //prints to excel
-	        try {
-				ExcelWriter.writeToExcel(CourseList.getCourseList());
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	    }
-	   
+	       
+	       JButton uploadButton = new JButton("Add & Parse Transcripts");
+	 
+	       uploadButton.setBounds(50, 200, 150, 30);
+	       panel.add(uploadButton);
+	       message = new JLabel("No files chosen.");
+	       panel.add(message);
+	       
+	       JButton excelButton = new JButton("Write Raw List to Excel");
+	       message2 = new JLabel("No Raw List spreadsheet yet.");
+	       panel.add(excelButton);
+	       panel.add(message2);
+	       
+	       GUI fileChooser = new GUI();
+	       uploadButton.addActionListener(fileChooser);
+	       excelButton.addActionListener(fileChooser);
+	       
+	       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	       frame.setVisible(true);
+	       }
 	    
+	    public void actionPerformed(ActionEvent e){
+	    	 String event = e.getActionCommand();  
+	         if (event.equals("Add & Parse Transcripts")) { 
+	             File directory = null;
+	             directory = TranscriptReader.getDirectory();
+	     			
+	             File[] transcriptSet = directory.listFiles();
+	     	     System.out.println("Transcript count: " + transcriptSet.length);
+	     	     TranscriptReader.parseTranscripts(transcriptSet);
+		     message.setText(transcriptSet.length + " transcripts successfully parsed."); //might need a better statement.
+
+	     	        //prints to console
+	     	     System.out.println(CourseList.printRawList());
+	         }
+	         if(event.equals("Write Raw List to Excel")){
+	        	  
+	        	  sortedList = CourseList.getCourseList();
+	        	  sortedList.sort(null);
+	        	  try {
+					ExcelWriter.writeToExcel(sortedList);
+					message2.setText("A Raw List spreadsheet has been created.");
+				} catch (FileNotFoundException e1) {
+					message.setText("Failed to write to excel. File not found.");
+					e1.printStackTrace();
+				} catch (IOException e1) {
+					message.setText("Failed to write to excel.");
+					e1.printStackTrace();
+				}
+	         }
+	    }
 }
