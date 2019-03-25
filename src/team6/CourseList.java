16f690e88f75c40c57addbad0faca043166595f9
ryannitz <ryan.nitz01@gmail.com>
2019-03-25 14:18:08 -0300

    test

---
+++ src/team6/CourseList.java
@@ -0,0 +1,65 @@
+package team6;
+/**
+ * @author: Ryan Nitz
+ */
+
+import java.util.ArrayList;
+
+class CourseList {
+	
+	/**
+	 * List to hold unique courses for Raw Distribution output
+	 */
+	public static ArrayList<Course> courseList = new ArrayList<Course>();
+
+	/**
+	 * Adds a course to the list if it is unique, increments a bin if it already exists.
+	 * @param gradeElements The elements of the grade read from the transcript
+	 * @return boolean If the course has been accounted for or not.
+	 */
+	public static boolean addCourse(ArrayList<String> gradeElements){
+		boolean courseExists = false;
+		int courseIndex = 0;
+		
+		for(int i = 0; i < courseList.size(); i++) {
+			if(courseList.get(i).getCourseNum().compareTo(gradeElements.get(0)) == 0) {
+				courseExists = true;
+				courseIndex = i;
+				break;
+			}
+		}
+		
+		if(courseExists) {
+			courseList.get(courseIndex).setGrade(gradeElements.get(3));
+		}else {
+			Course course = new Course(gradeElements);
+			courseList.add(course);
+		}
+		
+		return true;
+	}
+	
+	/**
+	 * @return The Raw course List
+	 */
+	public static ArrayList<Course> getCourseList(){
+		courseList.sort(null);
+		return courseList;
+	}
+	
+	/**
+	 * Prints the Raw Course alphabetically list in a text format.
+	 */
+	public static String printRawList() {
+		//TODO: Change this to output to a .txt file.
+		courseList.sort(null);
+		
+		String template = "Raw course List: \n";
+		template += "\t\t\tOther   Fails Marginal  Meets  Exceeds \n";
+		for(Course course : CourseList.courseList) {
+				template += course.toString();
+		}
+		return template;
+		
+	}
+}
---
+++ src/team6/GUI.java
@@ -0,0 +1,81 @@
+package team6;
+<<<<<<< HEAD
+
+=======
+>>>>>>> be98c5314b46fd8f16b385620c30d3cae8bebc33
+/**
+ * @author Uwera Ntaganzwa
+ */
+import javax.swing.*; 
+import java.awt.event.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class GUI extends JFrame implements ActionListener {
+		private static JLabel message;
+		private static JLabel message2;
+		ArrayList<Course> sortedList = new ArrayList<>();
+		GUI(){}
+	    public static void main(String args[]){
+	       JFrame frame = new JFrame("Student Transcript Analyser");
+	       JPanel panel = new JPanel();
+	       frame.setSize(400,350);
+	       frame.add(panel);
+
+	       
+	       JButton uploadButton = new JButton("Add & Parse Transcripts");
+	 
+	       uploadButton.setBounds(50, 200, 150, 30);
+	       panel.add(uploadButton);
+	       message = new JLabel("No files chosen.");
+	       panel.add(message);
+	       
+	       JButton excelButton = new JButton("Write Raw List to Excel");
+	       message2 = new JLabel("No Raw List spreadsheet yet.");
+	       panel.add(excelButton);
+	       panel.add(message2);
+	       
+	       GUI fileChooser = new GUI();
+	       uploadButton.addActionListener(fileChooser);
+	       excelButton.addActionListener(fileChooser);
+	       
+	       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	       frame.setVisible(true);
+	       }
+	    
+	    public void actionPerformed(ActionEvent e){
+	    	 String event = e.getActionCommand();  
+	         if (event.equals("Add & Parse Transcripts")) { 
+	             File directory = null;
+	             directory = TranscriptReader.getDirectory();
+	     			
+	             File[] transcriptSet = directory.listFiles();
+	     	     System.out.println("Transcript count: " + transcriptSet.length);
+	     	     TranscriptReader.parseTranscripts(transcriptSet);
+		     message.setText(transcriptSet.length + " transcripts successfully parsed."); //might need a better statement.
+
+	     	        //prints to console
+	     	     System.out.println(CourseList.printRawList());
+	         }
+	         if(event.equals("Write Raw List to Excel")){
+	        	  
+	        	  sortedList = CourseList.getCourseList();
+<<<<<<< HEAD
+=======
+	        	  sortedList.sort(null);
+>>>>>>> be98c5314b46fd8f16b385620c30d3cae8bebc33
+	        	  try {
+					ExcelWriter.writeToExcel(sortedList);
+					message2.setText("A Raw List spreadsheet has been created.");
+				} catch (FileNotFoundException e1) {
+					message.setText("Failed to write to excel. File not found.");
+					e1.printStackTrace();
+				} catch (IOException e1) {
+					message.setText("Failed to write to excel.");
+					e1.printStackTrace();
+				}
+	         }
+	    }
+}
