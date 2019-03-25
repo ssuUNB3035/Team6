f44ac89fb658f8a0018176a33e509100613f6062
Stephen Keith LeBlanc <skjleblanc@hotmail.ca>
2019-03-25 13:28:43 -0400

    Test push

---
+++ AreaDistribution.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+class CourseArea {
+    private int fails = 0, marginals = 0, meets = 0, exceeds = 0;
+
+    public int getFails() {
+        return fails;
+    }
+
+    public int getMarginals() {
+        return marginals;
+    }
+
+    public int getMeets() {
+        return meets;
+    }
+
+    public int getsExceeds() {
+        return exceeds;
+    }
+
+    public void addFail() {
+        fails++;
+    }
+}
+
+public class AreaDistribution {
+    private ArrayList<String> courseSections = new ArrayList<String>();
+
+
+
+
+}
