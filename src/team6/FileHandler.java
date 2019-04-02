6f74d78c606f0a1304cbabbbdc3f6ae7b03b24ea
Uwera <nnuwera@gmail.com>
2019-04-02 11:31:33 -0300

    removed addConfigFile, FileHandler modified; only one excel workbook required for different methods.

--- src/team6/FileHandler.java
+++ src/team6/FileHandler.java
@@ -4,56 +4,26 @@ package team6;
  * @author Ryan Nitz
  */
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
+
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import org.apache.poi.hssf.usermodel.*;
 
 public class FileHandler {
-	private static ArrayList<String> createdFiles = new ArrayList<>();
 	
-	//Can this be replace with a file.exists method?		
-	public static boolean findFile(String name){
-		boolean success = false;
-		for(String names : createdFiles) {
-			if(names == "Results.xslx") {
-				//then the file exists and can be written to
-				success = true;
-			}
-		}
-		if(success == false) {
-			String fName = "Results.xslx";
-			createdFiles.add(fName);
-			success = true;
-		}
-		return success;
-	}
-	public static void addConfigFile(File file) throws IOException {
-		File configFile = file;
-		BufferedReader reader = new BufferedReader(new FileReader(file)); 
-		BufferedWriter writer = new BufferedWriter(new FileWriter(configFile));
-		String str; 
-		while ((str = reader.readLine()) != null) { 
-		    writer.write(str);
-		}
-		reader.close();
-		writer.close();
-	}
-		
+	static HSSFWorkbook workbook = new HSSFWorkbook();
+	
+	/**
+	 * 	
+	 * @param sortedList The Raw List of courses to be written in Excel
+	 * @throws FileNotFoundException 
+	 * @throws IOException
+	 */
 	public static void writeRawList(ArrayList<Course> sortedList) throws FileNotFoundException, IOException {
-		findFile("Results.xslx");
-		HSSFWorkbook workbook = new HSSFWorkbook();
 		HSSFSheet sheet = workbook.createSheet("Raw List");
 		HSSFRow row = sheet.createRow(0);
-		
 		String columnHeaders[] = {"Course Number", "Course Name", "Others", "Fails", "Marginals","Meets", "Exceeds"};
 		for(int c = 0; c < columnHeaders.length; c++) {
 			HSSFCell cell = row.createCell(c);
@@ -77,13 +47,12 @@ public class FileHandler {
 			}
 		}
 		
-		workbook.write(new FileOutputStream("Results.xslx"));
+		workbook.write(new FileOutputStream("Results.xsl"));
 		workbook.close();
 		//System.out.println("Courses have been successfully copied to the Raw List sheet.");
 	}
 	
 	public static void writeRawList(ArrayList<Course> sortedList, String fileName) throws FileNotFoundException, IOException {
-		findFile(fileName);
 		HSSFWorkbook workbook = new HSSFWorkbook();
 		HSSFSheet sheet = workbook.createSheet("Raw List");
 		
