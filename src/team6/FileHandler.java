ec44258b8228b3096f66da12fe0c55c41bb030f4
Uwera <nnuwera@gmail.com>
2019-04-02 18:28:17 -0300

    Results.xslx created, .xsl file deleted. HSSF workbook, sheets, rows & cells changed to XSSF.

--- src/team6/FileHandler.java
+++ src/team6/FileHandler.java
@@ -33,7 +33,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class FileHandler {
 	
-	static HSSFWorkbook workbook = new HSSFWorkbook();
+	static XSSFWorkbook workbook = new XSSFWorkbook();
 	
 	/**
 	 * 	
@@ -42,63 +42,63 @@ public class FileHandler {
 	 * @throws IOException
 	 */
 	public static void writeRawList(ArrayList<Course> sortedList) throws FileNotFoundException, IOException {
-		HSSFSheet sheet = workbook.createSheet("Raw List");
-		HSSFRow row = sheet.createRow(0);
+		XSSFSheet sheet = workbook.createSheet("Raw List");
+		XSSFRow row = sheet.createRow(0);
 		String columnHeaders[] = {"Course Number", "Course Name", "Others", "Fails", "Marginals","Meets", "Exceeds"};
 		for(int c = 0; c < columnHeaders.length; c++) {
-			HSSFCell cell = row.createCell(c);
+			XSSFCell cell = row.createCell(c);
 			cell.setCellValue(columnHeaders[c]);
 		}
 		
 		int n = 0, m = 0;
 		for(Course courseIn: sortedList) {
 			n++;
-			HSSFRow nextRow = sheet.createRow(n);
+			XSSFRow nextRow = sheet.createRow(n);
 			
-			HSSFCell numCell = nextRow.createCell(m);
-			HSSFCell nameCell = nextRow.createCell(m+1);
+			XSSFCell numCell = nextRow.createCell(m);
+			XSSFCell nameCell = nextRow.createCell(m+1);
 			numCell.setCellValue(courseIn.getCourseNum());
 			nameCell.setCellValue(courseIn.getCourseName());
 			
 			int[] levels = courseIn.getLevels();
 			for(int c = 0; c < levels.length; c++) {
-				HSSFCell cell = nextRow.createCell(c+2);
+				XSSFCell cell = nextRow.createCell(c+2);
 				cell.setCellValue(levels[c]);
 			}
 		}
 		
-		workbook.write(new FileOutputStream("Results.xsl"));
+		workbook.write(new FileOutputStream("Results.xslx"));
 		workbook.close();
 		//System.out.println("Courses have been successfully copied to the Raw List sheet.");
 	}
 	
 	public static void writeRawList(ArrayList<Course> sortedList, String fileName) throws FileNotFoundException, IOException {
-		HSSFWorkbook workbook = new HSSFWorkbook();
-		HSSFSheet sheet = workbook.createSheet("Raw List");
+		XSSFWorkbook workbook = new XSSFWorkbook();
+		XSSFSheet sheet = workbook.createSheet("Raw List");
 		
-		HSSFRow row = sheet.createRow(0);
+		XSSFRow row = sheet.createRow(0);
 		
 
 		String columnHeaders[] = {"Course Number", "Course Name", "Others", "Fails", "Marginal","Meets", "Exceeds"};
 
 		for(int c = 0; c < columnHeaders.length; c++) {
-			HSSFCell cell = row.createCell(c);
+			XSSFCell cell = row.createCell(c);
 			cell.setCellValue(columnHeaders[c]);
 		}
 		
 		int n = 0, m = 0;
 		for(Course courseIn: sortedList) {
 			n++;
-			HSSFRow nextRow = sheet.createRow(n);
+			XSSFRow nextRow = sheet.createRow(n);
 			
-			HSSFCell numCell = nextRow.createCell(m);
-			HSSFCell nameCell = nextRow.createCell(m+1);
+			XSSFCell numCell = nextRow.createCell(m);
+			XSSFCell nameCell = nextRow.createCell(m+1);
 			numCell.setCellValue(courseIn.getCourseNum());
 			nameCell.setCellValue(courseIn.getCourseName());
 			
 			int[] levels = courseIn.getLevels();
 			for(int c = 0; c < levels.length; c++) {
-				HSSFCell cell = nextRow.createCell(c+2);
+				XSSFCell cell = nextRow.createCell(c+2);
 				cell.setCellValue(levels[c]);
 			}
 		}
