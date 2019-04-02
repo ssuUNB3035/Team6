7c281a314848c413ead18bc5a3ac4bf3b4da2b0d
ryannitz <ryan.nitz01@gmail.com>
2019-04-02 11:15:05 -0300

    Javadoc comments

--- src/team6/Cohort.java
+++ src/team6/Cohort.java
@@ -3,26 +3,31 @@ package team6;
 import java.util.ArrayList;
 
 /**
- * 
  * @author Ryan Nitz
- *
  */
 
 public class Cohort {
+	
 	private int firstYearCount;
 	private int secondYearCount;
 	private int thirdYearCount;
 	private int fourthYearCount;
-	
 	private String cohortName;
-	
 	private ArrayList<Transcript> transcripts;
 	
+	/**
+	 * @param cohortName - The name of the cohort file.
+	 */
 	public Cohort(String cohortName) {
 		this.cohortName = cohortName;
 		this.transcripts = new ArrayList<Transcript>();
 	}
 	
+	/**
+	 * Adds a transcript to the cohort object and determines the year of the student.
+	 * @param transcript - The transcript being added
+	 * @return boolean - If the transcript wad added to the list successfully.
+	 */
 	public boolean addTranscript(Transcript transcript) {
 		double creditHours = 0.00;
 		if(transcripts.add(transcript)) {
@@ -36,37 +41,59 @@ public class Cohort {
 			}else if(creditHours >= 120){
 				fourthYearCount++;
 			}
+			return true;
 		}
 		
 		return false;
 	}
 	
+	/**
+	 * @return cohortName - The name of the cohort
+	 */
 	public String getCohortName() {
 		return this.cohortName;
 	}
 	
-	
+	/**
+	 * @return firstYearCount - The number of transcripts that are in their first year.
+	 */
 	public int getFirstYearCount() {
 		return firstYearCount;
 	}
 	
+	/**
+	 * @return secondYearCount - The number of transcripts that are in their second year.
+	 */
 	public int getSecondYearCount() {
 		return secondYearCount;
 	}
 	
+	/**
+	 * @return thirdYearCount - The number of transcripts that are in their third year.
+	 */
 	public int getThirdYearCount() {
 		return thirdYearCount;
 	}
 	
+	/**
+	 * @return	fourthYearCount - The number of transcripts that in their fourth year.
+	 */
 	public int getFourthYearCount() {
 		return fourthYearCount;
 	}
 	
+	/**
+	 * @return yearCounts[] - The distribution of the students in each year. 
+	 */
 	public int[] getYearDistribution() {
 		int yearCounts[] = {firstYearCount, secondYearCount, thirdYearCount, fourthYearCount};
 		return yearCounts;
 	}
 	
+	/**
+	 * 
+	 * @return 
+	 */
 	public String toString() {
 		String template = "Students in First Year: " + this.firstYearCount + "\n"
 				+"Students in Second Year: " + this.secondYearCount + "\n"
