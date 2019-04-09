56df748028882c797c2b156a27f16efcecf31aeb
Uwera <nnuwera@gmail.com>
2019-04-09 19:32:58 -0300

    added comments to GUI.

--- src/team6/GUI.java
+++ src/team6/GUI.java
@@ -11,26 +11,71 @@ import java.util.ArrayList;
 
 
 public class GUI extends JFrame implements ActionListener {
+		/**
+		 * Set of transcripts parsed in one run
+		 */
 		Cohort cohort;
-		private static String fileDictName = "";
+		/**
+		 * The name of the directory from which a results file is selected
+		 */
+		private static String directoryName = "";
+		/**
+		 * The file containing area and equivalences and to which results are written
+		 */
 		private static String fileName = readInFile();
+		/**
+		 * The number of times the parse button is clicked
+		 */
 		private static int parseCount;
+		/**
+		 * The number of times the write to excel button is clicked
+		 */
 		private static int writeCount;
+		/**
+		 * Button for parsing a given set of transcripts
+		 */
 		private static JButton parseButton;
+		/**
+		 * Button for writing results to an excel file
+		 */
 		private static JButton excelButton;
+		/**
+		 * Button for retrieve stored files
+		 */
 		private static JButton retrieveFilesButton;
+		/**
+		 * The message displayed when the parseButton is clicked
+		 */
 		private static JLabel message;
+		/**
+		 * The message displayed when the excelButton is clicked
+		 */
 		private static JLabel message2;
+		/**
+		 * The message displayed when the retrieve files button is clicked
+		 */
 		private static JLabel retrieveMessage;
+		/**
+		 * The names of the files found when the retrieveFilesButton is clicked
+		 */
 		private static JLabel listedFiles;
+		/**
+		 * A raw list of courses created when transcripts have been parsed successfully
+		 */
 		ArrayList<Course> sortedList = new ArrayList<>();
+		
 		GUI(){}
+		
+		
 	    public static void main(String args[]){
 	       JFrame frame = new JFrame("Student Transcript Analyser");
 	       JPanel panel = new JPanel();
 	       frame.setSize(500,450);
 	       frame.add(panel);
 	       
+	       /**
+			* Initializes parseButton and sets a default message corresponding to the action performed on the button
+			*/
 	       parseButton = new JButton("Parse Transcripts");
 	       parseButton.setBounds(50, 200, 150, 30);
 	       panel.add(parseButton);
@@ -38,7 +83,9 @@ public class GUI extends JFrame implements ActionListener {
 	       panel.add(message);
 	       parseCount =0;
 	       
-	       
+	       /**
+			* Initializes & hides the write to excel button and sets a default message corresponding to the action performed on the button
+			*/
 	       excelButton = new JButton("Write Results to Excel");
 	       message2 = new JLabel("No workbook created yet.");
 	       panel.add(excelButton);
@@ -47,6 +94,9 @@ public class GUI extends JFrame implements ActionListener {
 	       excelButton.setVisible(false);
 	       message2.setVisible(false);
 	       
+	       /**
+			* Initializes & hides the retrieveFilesButton and sets a default message corresponding to the action performed on the button
+			*/
 	       retrieveFilesButton = new JButton("Retrieve Stored Files");
 	       retrieveMessage = new JLabel("");
 	       listedFiles = new JLabel("");
@@ -57,6 +107,9 @@ public class GUI extends JFrame implements ActionListener {
 	       retrieveMessage.setVisible(false);
 	       listedFiles.setVisible(false);
 	       
+	       /**
+	        * Adds event listeners for all buttons
+	        */
 	       GUI fileChooser = new GUI();
 	       parseButton.addActionListener(fileChooser);
 	       excelButton.addActionListener(fileChooser);
@@ -68,7 +121,11 @@ public class GUI extends JFrame implements ActionListener {
 	    
 	    public void actionPerformed(ActionEvent e){
 	    	 String event = e.getActionCommand(); 
-
+	    	 /**
+	    	  * Parses Transcripts when parseButton is clicked
+	    	  * @throws IllegalArgumentException - When the user tries to parse the same transcript multiple times
+	    	  * @throws IOException - When transcripts cannot be opened
+	    	  */
 	         if (event.equals("Parse Transcripts")) { 
 	            if (parseCount > 0) {
 		        	message.setText("Transcripts in this cohort have already been parsed.");
@@ -95,12 +152,17 @@ public class GUI extends JFrame implements ActionListener {
 
 		     	     parseCount++; 
 		        } catch (IllegalArgumentException e1) {
-		        	message.setText("Error parsing transcripts. One or more files may be corrupted.");
+		        	message.setText("Error parsing transcripts. This cohort may have already been parsed.");
 		        } catch (IOException e1) {
 					e1.printStackTrace();
 				}
 	         }
-	         
+	         /**
+	          * Writes results to specified output file when clicked
+	          * @throws FileNotFoundException - When the output file is not found
+	          * @throws IllegalArgumentException - When the output file has already been written to
+	          * @throws IOException - When specified file cannot be opened/read
+	          */
 	         if(event.equals("Write Results to Excel")){
 	        	 if (writeCount > 0) {
 			        	message.setText("A results workbook has already been created.");
@@ -126,14 +188,17 @@ public class GUI extends JFrame implements ActionListener {
 					e1.printStackTrace();
 				}
 	         }
-	         
+	         /**
+	          * Lists the files in the system available to the user for further processing
+	          */
 	         if(event.equals("Retrieve Stored Files")){
-	        	 retrieveMessage.setText("The following files are available for further processing:\n");
-	        	 FileHandler.retrieveStoredFiles();
-	        	 
+	        	 retrieveMessage.setText("The following files are available for further processing:\n" + FileHandler.retrieveStoredFiles());	 
 	         }
 }
-	    
+	    /**
+	     * Allows a user to select an output file
+	     * @return name - The output file selected when the program starts
+	     */
 	    private static String readInFile() {
 	    	String source = System.getProperty("user.dir");
 	    	JFileChooser fileChooser = new JFileChooser();
@@ -146,7 +211,7 @@ public class GUI extends JFrame implements ActionListener {
             int result = fileChooser.showOpenDialog(fileChooser);
             
             if (result == JFileChooser.APPROVE_OPTION) {
-                fileDictName = fileChooser.getSelectedFile().getAbsolutePath();
+                directoryName = fileChooser.getSelectedFile().getAbsolutePath();
             } else {
                 return "NULL";
             }
